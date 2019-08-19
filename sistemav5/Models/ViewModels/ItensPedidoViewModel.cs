5bbe17ff7df69c2ffad3ce2c7cd8b6b2e6f1ce11
Wellington <well.raiser@gmail.com>
2019-08-19 16:18:28 -0300

    v5.1.4

---
+++ sistemav5/Models/ViewModels/ItensPedidoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistemav5.Models.ViewModels
+{
+    public class ItensPedidoViewModel
+    {
+        public ICollection<Produto> Produtos { get; set; }
+        public ItensPedido ItensPedido { get; set; }
+
+        List<Produto> ViewList { get; set; } = new List<Produto>();
+
+        public void AddProdutoVM(Produto p,int quantidade)
+        {
+            ViewList.Add(p);
+            p.Quantidade -= quantidade;
+        }
+    }
+}
